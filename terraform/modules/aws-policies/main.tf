bcc04a540c83412f1d111b2c6f46222e85080952
Grzegorz Rygielski <grzegorz.rygielski@ottogroup.com>
2023-04-14 14:08:31 +0200

    aws-policies: add ViewOnlyAccess

--- terraform/modules/aws-policies/main.tf
+++ terraform/modules/aws-policies/main.tf
@@ -42,10 +42,11 @@ resource "aws_iam_role_policy_attachment" "attach_SecurityAudit_to_gcp_federatio
   role       = aws_iam_role.gcp_federation.name
   policy_arn = data.aws_iam_policy.SecurityAudit.arn
 }
-data "aws_iam_policy" "ViewOnly" {
-  name = "ViewOnly"
+data "aws_iam_policy" "ViewOnlyAccess" {
+  name = "ViewOnlyAccess"
 }
-resource "aws_iam_role_policy_attachment" "attach_ViewOnly_to_gcp_federation" {
+resource "aws_iam_policy_attachment" "attach_ViewOnlyAccess_to_gcp_federation" {
   role       = aws_iam_role.gcp_federation.name
-  policy_arn = data.aws_iam_policy.ViewOnly.arn
+  policy_arn = data.aws_iam_policy.ViewOnlyAccess.arn
+  name       = "pantheon-has-view-only-access"
 }
