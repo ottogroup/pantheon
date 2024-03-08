23779d1c7d71ba747447eccf86db1d7750a6ee28
Grzegorz Rygielski <grzegorz.rygielski@ottogroup.com>
2024-03-08 10:05:28 +0100

    aws-policies: change resource type for attach_ViewOnlyAccess_to_gcp_federation

--- terraform/modules/aws-policies/main.tf
+++ terraform/modules/aws-policies/main.tf
@@ -47,10 +47,9 @@ resource "aws_iam_role_policy_attachment" "attach_SecurityAudit_to_gcp_federatio
 data "aws_iam_policy" "ViewOnlyAccess" {
   name = "ViewOnlyAccess"
 }
-resource "aws_iam_policy_attachment" "attach_ViewOnlyAccess_to_gcp_federation" {
+resource "aws_iam_role_policy_attachment" "attach_ViewOnlyAccess_to_gcp_federation" {
+  role      = aws_iam_role.gcp_federation.name
   policy_arn = data.aws_iam_policy.ViewOnlyAccess.arn
-  name       = "pantheon-has-view-only-access"
-  roles      = [aws_iam_role.gcp_federation.name]
 }
 
 data "aws_iam_policy_document" "override" {
